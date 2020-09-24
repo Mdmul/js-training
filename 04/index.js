1e68f4f750b82a13fd3d6d28a1f7fc17552fbd9e
dima <oljamymail@gmail.com>
2020-09-24 11:15:35 +0300

    Add homework 04

---
+++ 04/index.js
@@ -0,0 +1,23 @@
+// Задача 1
+
+
+const text = document.querySelector('.text');
+text.style.padding = '20px';
+text.style.backgroundColor = 'red';
+text.style.color = 'white';
+
+
+
+// Задача 3
+
+const message = document.querySelector('.message');
+message.style.backgroundColor = 'bk';
+document.p = 'Всем привет';
+message.style.top = 'top';
+message.style.left = 'left';
+
+    function showMessage(bk, top, left) {
+        console.log('message')
+    }
+showMessage('yellow', 100, 50)
+
